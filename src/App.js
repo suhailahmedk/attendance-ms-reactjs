af09edc6d8ca004af9ba8e77db0f5bf7642accfd
suhailahmedk <suhailahmed.se17@iba-suk.edu.pk>
2023-08-19 12:20:21 +0500

    starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import PersonTable from './PersonTable';
+import MonthCheckBox from './MonthCheckBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from "react-bootstrap/Table";
+import FormCheck from "react-bootstrap/FormCheck"
+import { Container } from 'react-bootstrap';
+import moment from "moment";
 
 function App() {
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=10")
+      .then((res) => res.json())
+      .then((json) => json.results)
+      .then((people) => people.map((person) => person.name.first))
+      .then((names) => console.log(names));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <PersonTable></PersonTable>
+      
+   
   );
 }
 
